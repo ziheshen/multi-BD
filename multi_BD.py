b056015e77931ea812803a4a06e603c9ec513e88
ziheshen <shenzihe1011@gmail.com>
2024-06-12 12:34:19 +0000

    add generate image func

--- multi_BD.py
+++ multi_BD.py
@@ -2,21 +2,21 @@ import torch
 import torch.nn.functional as F
 import torch.utils.checkpoint
 from torchvision import transforms
-from tqdm.auto import tqdm
+# from tqdm.auto import tqdm
 from transformers import CLIPTokenizer, PretrainedConfig
 import open_clip
 from disen_net import Image_adapter, cal_cos
 import torch.nn as nn
 import logging
 import os
+import tqdm
 
 import diffusers
 from diffusers import (
     AutoencoderKL,
     DDPMScheduler,
-    DiffusionPipeline,
-    DPMSolverMultistepScheduler,
-    StableDiffusionPipeline,
+    DDIMScheduler,
+    PNDMScheduler,
     UNet2DConditionModel,
 )
 # from diffusers.loaders import (
@@ -37,6 +37,11 @@ from diffusers.models.attention_processor import (
 from lavis.models.blip2_models.blip2_qformer import Blip2Qformer
 from lavis.models.blip_diffusion_models.modeling_ctx_clip import CtxCLIPTextModel
 from lavis.common.utils import download_and_untar, is_url
+from lavis.models.blip_diffusion_models.ptp_utils import (
+    LocalBlend,
+    P2PCrossAttnProcessor,
+    AttentionRefine,
+)
 from transformers.activations import QuickGELUActivation as QuickGELU
 
 class ProjLayer(nn.Module):
@@ -250,6 +255,26 @@ class MultiBlipDisenBooth(nn.Module):
         logging.info("Loading pretrained unet weights.")
         load_state_dict(self.unet, "unet/diffusion_pytorch_model.bin")
 
+    @property
+    def pndm_scheduler(self):
+        if not hasattr(self, "_pndm_scheduler"):
+            self._pndm_scheduler = PNDMScheduler(
+                beta_start=0.00085,
+                beta_end=0.012,
+                beta_schedule="scaled_linear",
+                set_alpha_to_one=False,
+                skip_prk_steps=True,
+            )
+        return self._pndm_scheduler
+
+    @property
+    def ddim_scheduler(self):
+        if not hasattr(self, "_ddim_scheduler"):
+            self._ddim_scheduler = DDIMScheduler.from_config(
+                "runwayml/stable-diffusion-v1-5", subfolder="scheduler"
+            )
+        return self._ddim_scheduler
+
     def before_training(self, dataset, **kwargs):
         # print(dataset)
         # assert len(dataset) == 1, "Only support single dataset for now."
@@ -323,7 +348,7 @@ class MultiBlipDisenBooth(nn.Module):
         ### Identity-irrelevent Branch ###
         # Get the image embedding
         img_embeddings = self.img_encoder.encode_image( self.clip_trans(samples["inp_image"]) ).unsqueeze(1)
-        img_embeddings.mean(dim=0, keepdim=True)
+        # img_embeddings.mean(dim=0, keepdim=True)
         img_state = self.img_adapter( img_embeddings )
 
         # Predict the noise residual
@@ -342,7 +367,7 @@ class MultiBlipDisenBooth(nn.Module):
 
         # Get the target for loss depending on the prediction type
         if self.noise_scheduler.config.prediction_type == "epsilon":
-                    target = noise
+            target = noise
         elif self.noise_scheduler.config.prediction_type == "v_prediction":
             target = self.noise_scheduler.get_velocity(latents, noise, timesteps)
         else:
@@ -396,3 +421,274 @@ class MultiBlipDisenBooth(nn.Module):
             # print('\n\n================================')
         
         return ctx_embeddings
+
+    @torch.no_grad()
+    def generate(
+            self,
+            samples,
+            latents=None,
+            guidance_scale=7.5,
+            height=512,
+            width=512,
+            seed=42,
+            num_inference_steps=50,
+            neg_prompt="",
+            controller=None,
+            prompt_strength=1.0,
+            prompt_reps=20,
+            use_ddim=False,
+            text_weight = 1.0,
+            image_weight = 0.0,
+    ):
+        if controller is not None:
+            self._register_attention_refine(controller)
+        
+        cond_image = samples["cond_images"]  # reference image
+        cond_subject = samples["cond_subject"]  # source subject category
+        tgt_subject = samples["tgt_subject"]  # target subject category
+        prompt = samples["prompt"]
+        cldm_cond_image = samples.get("cldm_cond_image", None)  # conditional image
+
+        prompt = self._build_prompt(
+            prompts=prompt,
+            tgt_subjects=tgt_subject,
+            prompt_strength=prompt_strength,
+            prompt_reps=prompt_reps,
+        )
+
+        text_embeddings = self._forward_prompt_embeddings(
+            cond_image, cond_subject, prompt
+        )
+
+        img_feature = self.img_encoder.encode_image( cond_image ).unsqueeze(1) 
+        if self.img_adapter is not None:
+            img_feature = self.img_adapter(img_feature)
+        img_feature = torch.cat([torch.zeros_like(img_feature),img_feature])
+
+        # 3. unconditional embedding
+        do_classifier_free_guidance = guidance_scale > 1.0
+        if do_classifier_free_guidance:
+            max_length = self.text_encoder.text_model.config.max_position_embeddings
+
+            uncond_input = self.tokenizer(
+                [neg_prompt],
+                padding="max_length",
+                max_length=max_length,
+                return_tensors="pt",
+            )
+            uncond_embeddings = self.text_encoder(
+                input_ids=uncond_input.input_ids.to(self.device()),
+                ctx_embeddings=None,
+            )[0]
+
+            # For classifier free guidance, we need to do two forward passes.
+            # Here we concatenate the unconditional and text embeddings into a single batch
+            # to avoid doing two forward passes
+            text_embeddings = torch.cat([uncond_embeddings, text_embeddings])
+        
+        prompt_embeds = text_weight*prompt_embeds + image_weight*img_feature
+
+        if seed is not None:
+            generator = torch.Generator(device=self.device())
+            generator = generator.manual_seed(seed)
+
+        latents = self._init_latent(latents, height, width, generator, batch_size=1)
+
+        scheduler = self.pndm_scheduler if not use_ddim else self.ddim_scheduler
+
+        # set timesteps
+        extra_set_kwargs = {}
+        scheduler.set_timesteps(num_inference_steps, **extra_set_kwargs)
+
+        iterator = tqdm.tqdm(scheduler.timesteps)
+
+        for i, t in enumerate(iterator):
+            latents = self._denoise_latent_step(
+                latents=latents,
+                t=t,
+                text_embeddings=text_embeddings,
+                cond_image=cldm_cond_image,
+                height=height,
+                width=width,
+                guidance_scale=guidance_scale,
+                use_inversion=use_ddim,
+            )
+
+        image = self._latent_to_image(latents)
+
+        return image
+
+    def _denoise_latent_step(
+        self,
+        latents,
+        t,
+        text_embeddings,
+        guidance_scale,
+        height,
+        width,
+        cond_image=None,
+        use_inversion=False,
+    ):
+        if use_inversion:
+            noise_placeholder = []
+
+        # expand the latents if we are doing classifier free guidance
+        do_classifier_free_guidance = guidance_scale > 1.0
+
+        latent_model_input = (
+            torch.cat([latents] * 2) if do_classifier_free_guidance else latents
+        )
+
+        # predict the noise residual
+        noise_pred = self._predict_noise(
+            t=t,
+            latent_model_input=latent_model_input,
+            text_embeddings=text_embeddings,
+            width=width,
+            height=height,
+            cond_image=cond_image,
+        )
+
+        if use_inversion:
+            noise_placeholder.append(noise_pred[2].unsqueeze(0))
+
+        # perform guidance
+        if do_classifier_free_guidance:
+            noise_pred_uncond, noise_pred_text = noise_pred.chunk(2)
+            noise_pred = noise_pred_uncond + guidance_scale * (
+                noise_pred_text - noise_pred_uncond
+            )
+
+        if use_inversion:
+            noise_placeholder.append(noise_pred[-1].unsqueeze(0))
+            noise_pred = torch.cat(noise_placeholder)
+
+        # compute the previous noisy sample x_t -> x_t-1
+        scheduler = self.ddim_scheduler if use_inversion else self.pndm_scheduler
+
+        latents = scheduler.step(
+            noise_pred,
+            t,
+            latents,
+        )["prev_sample"]
+
+        return latents
+
+    def _register_attention_refine(
+        self,
+        src_subject,
+        prompts,
+        num_inference_steps,
+        cross_replace_steps=0.8,
+        self_replace_steps=0.4,
+        threshold=0.3,
+    ):
+        device, tokenizer = self.device, self.tokenizer
+
+        lb = LocalBlend(
+            prompts=prompts,
+            words=(src_subject,),
+            device=device,
+            tokenizer=tokenizer,
+            threshold=threshold,
+        )
+
+        controller = AttentionRefine(
+            prompts,
+            num_inference_steps,
+            cross_replace_steps=cross_replace_steps,
+            self_replace_steps=self_replace_steps,
+            tokenizer=tokenizer,
+            device=device,
+            local_blend=lb,
+        )
+
+        self._register_attention_control(controller)
+
+        return controller
+    
+    def _build_prompt(self, prompts, tgt_subjects, prompt_strength=1.0, prompt_reps=20):
+        rv = []
+        for prompt, tgt_subject in zip(prompts, tgt_subjects):
+            prompt = f"a {tgt_subject} {prompt.strip()}"
+            # a trick to amplify the prompt
+            rv.append(", ".join([prompt] * int(prompt_strength * prompt_reps)))
+
+        return rv
+    
+    def _forward_prompt_embeddings(self, input_image, src_subject, prompt):
+        # 1. extract BLIP query features and proj to text space -> (bs, 32, 768)
+        query_embeds = self.forward_ctx_embeddings(input_image, src_subject)
+
+        # 2. embeddings for prompt, with query_embeds as context
+        tokenized_prompt = self._tokenize_text(prompt).to(self.device)
+        text_embeddings = self.text_encoder(
+            input_ids=tokenized_prompt.input_ids,
+            ctx_embeddings=query_embeds,
+            ctx_begin_pos=[self._CTX_BEGIN_POS],
+        )[0]
+
+        return text_embeddings
+    
+    def _init_latent(self, latent, height, width, generator, batch_size):
+        if latent is None:
+            latent = torch.randn(
+                (1, self.unet.in_channels, height // 8, width // 8),
+                generator=generator,
+                device=generator.device,
+            )
+        latent = latent.expand(
+            batch_size,
+            self.unet.in_channels,
+            height // 8,
+            width // 8,
+        )
+        return latent.to(self.device)
+    
+    def _predict_noise(
+        self,
+        t,
+        latent_model_input,
+        text_embeddings,
+        width=512,
+        height=512,
+        cond_image=None,
+    ):
+        # if hasattr(self, "controlnet"):
+        #     cond_image = prepare_cond_image(
+        #         cond_image, width, height, batch_size=1, device=self.device
+        #     )
+
+        #     down_block_res_samples, mid_block_res_sample = self.controlnet(
+        #         latent_model_input,
+        #         t,
+        #         encoder_hidden_states=text_embeddings,
+        #         controlnet_cond=cond_image,
+        #         # conditioning_scale=controlnet_condition_scale,
+        #         return_dict=False,
+        #     )
+        # else:
+        #     down_block_res_samples, mid_block_res_sample = None, None
+        
+        down_block_res_samples, mid_block_res_sample = None, None
+
+        noise_pred = self.unet(
+            latent_model_input,
+            timestep=t,
+            encoder_hidden_states=text_embeddings,
+            down_block_additional_residuals=down_block_res_samples,
+            mid_block_additional_residual=mid_block_res_sample,
+        )["sample"]
+
+        return noise_pred
+    
+    def _latent_to_image(self, latents):
+        latents = 1 / 0.18215 * latents
+        image = self.vae.decode(latents).sample
+
+        image = (image / 2 + 0.5).clamp(0, 1)
+        image = image.cpu().permute(0, 2, 3, 1).numpy()
+
+        image = numpy_to_pil(image)
+
+        return image
