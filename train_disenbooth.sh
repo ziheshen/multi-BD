89bda6afdbe3b127075a13f64b4f7f1bba35f060
ziheshen <shenzihe1011@gmail.com>
2024-06-12 03:28:04 +0000

    .

--- train_disenbooth.sh
+++ train_disenbooth.sh
@@ -2,22 +2,22 @@
 export MODEL_NAME="runwayml/stable-diffusion-v1-5"
 export PRETRAINED_BLIP_DIFFUSION="https://storage.googleapis.com/sfr-vision-language-research/LAVIS/models/BLIP-Diffusion/blip-diffusion.tar.gz"
 export INSTANCE_DIR="/LAVIS/data/mit_mascot"
-export OUTPUT_DIR="./output/test"
+export OUTPUT_DIR="./output/with_without_irrel"
 
 accelerate launch train.py \
   --pretrained_model_name_or_path=$MODEL_NAME  \
   --pretrained_BLIPdiffusion_name_or_path=$PRETRAINED_BLIP_DIFFUSION \
   --instance_data_dir=$INSTANCE_DIR \
   --output_dir=$OUTPUT_DIR \
-  --subject_text="a mit_mascot" \
-  --resolution=1024 \
+  --subject_text="mit_mascot" \
+  --resolution=512 \
   --train_batch_size=1 \
   --gradient_accumulation_steps=1 \
-  --checkpointing_steps=200 \
+  --checkpointing_steps=20 \
   --learning_rate=1e-4 \
   --lr_scheduler="constant" \
   --lr_warmup_steps=0 \
-  --max_train_steps=2000 \
+  --max_train_steps=200 \
   --validation_prompt="A mit_mascot in the jungle" \
-  --validation_epochs=200 \
+  --validation_epochs=20 \
   --seed="0" \
