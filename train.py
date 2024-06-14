af7ed138cd09d564a99033b41f83a8a49c86e803
ziheshen <shenzihe1011@gmail.com>
2024-06-14 09:56:49 +0000

    get blip feature before training

--- train.py
+++ train.py
@@ -17,6 +17,7 @@ import torch
 import torch.nn.functional as F
 import torch.utils.checkpoint
 import transformers
+from transformers import CLIPTokenizer
 from accelerate import Accelerator
 from accelerate.logging import get_logger
 from accelerate.utils import ProjectConfiguration, set_seed
@@ -61,7 +62,7 @@ from lavis.models.blip_diffusion_models.modeling_ctx_clip import CtxCLIPTextMode
 from multi_BD import MultiBlipDisenBooth
 from dataset import SubjectDrivenTextToImageDataset, collate_fn
 from transformers.activations import QuickGELUActivation as QuickGELU
-from utils import parse_args
+from utils import train_parse_args
 
 logger = get_logger(__name__)
 
@@ -118,6 +119,7 @@ def train(args):
 
     # If passed along, set the training seed now.
     if args.seed is not None:
+        print("Seed: ", args.seed)
         set_seed(args.seed)
 
     if accelerator.mixed_precision == "fp16":
@@ -131,9 +133,9 @@ def train(args):
     model = MultiBlipDisenBooth(args)
     # with open('/LAVIS/multi_BlipDisenBooth/multi_BLIP_DisenBooth.txt', 'w') as f:
     #         for name, param in model.named_parameters():
-    #             f.write(f"{name}\n")
+    #             f.write(f"{name}:\n")
     #             f.write(f"{param}\n")
-    
+    # return
     # if args.load_model is not None:
     #     model.load_state_dict(
     #         torch.load(Path(args.load_model) / "pytorch_model.bin", map_location="cpu")
@@ -143,6 +145,7 @@ def train(args):
         model.text_encoder.requires_grad(args.train_text_encoder)
 
     model.to(accelerator.device, dtype = weight_dtype)
+    # model.to(accelerator.device)
 
     if args.enable_xformers_memory_efficient_attention:
         if is_xformers_available():
@@ -159,8 +162,6 @@ def train(args):
 
     if args.gradient_checkpointing:
         model.unet.enable_gradient_checkpointing()
-        model.img_adapter.enable_gradient_checkpointing()
-        model.proj_layer.enable_gradient_checkpointing()
         if args.train_text_encoder:
             model.text_encoder.gradient_checkpointing_enable()
     
@@ -180,16 +181,19 @@ def train(args):
     optimizer_class = torch.optim.AdamW
 
     unet_params = list([p for p in model.unet.parameters() if p.requires_grad])
-    proj_layer_params = list([p for p in model.proj_layer.parameters() if p.requires_grad])
-    img_adapter_params = list([p for p in model.img_adapter.parameters() if p.requires_grad])
+    
     if args.train_text_encoder:
         text_encoder_params = list([p for p in model.text_encoder.parameters() if p.requires_grad])
-    
-    optimizer_params = [
-        {"params": unet_params, "lr": args.learning_rate * args.unet_lr_scale},
-        {"params": proj_layer_params, "lr": args.learning_rate * args.proj_lr_scale},
-        {"params": img_adapter_params, "lr": args.learning_rate * args.img_adapter_lr_scale},
-    ]
+    if args.use_irrel_branch:
+        img_adapter_params = list([p for p in model.img_adapter.parameters() if p.requires_grad])
+        optimizer_params = [
+            {"params": unet_params, "lr": args.learning_rate * args.unet_lr_scale},
+            {"params": img_adapter_params, "lr": args.learning_rate * args.img_adapter_lr_scale},
+        ]
+    else:
+        optimizer_params = [
+            {"params": unet_params, "lr": args.learning_rate * args.unet_lr_scale},
+        ]
 
     if args.train_text_encoder:
         text_encoder_params = [p for p in model.text_encoder.parameters() if p.requires_grad]
@@ -203,10 +207,14 @@ def train(args):
     )
 
     logging.info("Building datasets...")
+    tokenizer = CLIPTokenizer.from_pretrained(
+            args.pretrained_model_name_or_path, subfolder="tokenizer"
+        )
     train_dataset = SubjectDrivenTextToImageDataset(
         image_dir=args.instance_data_dir,
         subject_text=args.subject_text,
         text_prompt=args.text_prompt,
+        text_tokenizer=tokenizer,
     )
 
     train_dataloader = torch.utils.data.DataLoader(
@@ -284,9 +292,10 @@ def train(args):
         disable=not accelerator.is_local_main_process,
     )
 
+    model.before_training(model=model, dataset=train_dataset)
+    
     for epoch in range(first_epoch, args.num_train_epochs):
         model.train()
-        train_loss = 0.0
         for step, batch in enumerate(train_dataloader):
             progress_bar.set_description("Global step: {}".format(global_step))
 
@@ -303,9 +312,9 @@ def train(args):
                 accelerator.backward(loss)
                 if accelerator.sync_gradients:
                     params_to_clip = (
-                        unet_params + proj_layer_params + img_adapter_params +text_encoder_params
+                        unet_params + img_adapter_params +text_encoder_params
                         if args.train_text_encoder
-                        else unet_params + proj_layer_params + img_adapter_params
+                        else unet_params + img_adapter_params
                     )
                     accelerator.clip_grad_norm_(params_to_clip, args.max_grad_norm)
                 optimizer.step()
@@ -341,5 +350,5 @@ def train(args):
     accelerator.end_training()
 
 if __name__ == "__main__":
-    args = parse_args()
+    args = train_parse_args()
     train(args)
