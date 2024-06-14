a7b983873f60dac0b3cd98a4ebd2583cb9b9ff73
ziheshen <shenzihe1011@gmail.com>
2024-06-14 09:58:09 +0000

    inference

--- inference.py
+++ inference.py
@@ -1,24 +1,81 @@
-from utils import parse_args
+from utils import infer_parse_args
 from multi_BD import MultiBlipDisenBooth
 import torch
+from safetensors.torch import load_file
+import logging, time, os, sys
+from PIL import Image
 
 def inference(args):
+    logging_dir = "/LAVIS/multi_BlipDisenBooth/infer_logs"
+    if logging_dir is not None:
+            os.makedirs(logging_dir, exist_ok=True)
+
+    t = time.localtime()
+    str_m_d_y_h_m_s = time.strftime("%m-%d-%Y_%H-%M-%S", t)
+    logging.basicConfig(
+        format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
+        datefmt="%m/%d/%Y %H:%M:%S",
+        level=logging.INFO,
+        handlers=[
+            logging.StreamHandler(sys.stdout),
+            logging.FileHandler(
+                os.path.join(logging_dir, f"{str_m_d_y_h_m_s}.log")
+            ),
+        ]
+    )
 
     if torch.cuda.is_available():
         device = torch.device("cuda")
-        print(f"Using CUDA device: {torch.cuda.get_device_name(device)}")
+        logging.info(f"Using CUDA device: {torch.cuda.get_device_name(device)}")
     else:
         device = torch.device("cpu")
-        print("Using CPU device")
+        logging.info("Using CPU device")
 
     # Load model
     model = MultiBlipDisenBooth(args)
     model.to(device)
 
-    finetuned_ckpt = args.finetuned_ckpt
-    finetuned_ckpt = torch.load(finetuned_ckpt, map=device)
-    model.load_state_dict(finetuned_ckpt['model_state_dict'])
+    logging.info(f"Loading finetuned model from {args.finetuned_ckpt}...")
+    # finetuned_ckpt = args.finetuned_ckpt
+    # finetuned_ckpt = load_file(finetuned_ckpt, device="cpu")
+    # print(finetuned_ckpt.keys)
+    model.load_checkpoint(args.finetuned_ckpt)
+    # return
+    cond_image = Image.open("/LAVIS/data/mit_mascot/mit_mascot1.png")
+    cond_subject = ["mit_mascot"]
+    tgt_subject = ["mit_mascot"]
+    text_prompt = ["on the beach"]
+    samples = {
+        "cond_images": cond_image,
+        "cond_subject": cond_subject,
+        "tgt_subject": tgt_subject,
+        "prompt": text_prompt,
+    }
+
+    num_output = 4
+
+    iter_seed = 8888
+    guidance_scale = 7.5
+    num_inference_steps = 100
+    negative_prompt = "over-exposure, under-exposure, saturated, duplicate, out of frame, lowres, cropped, worst quality, low quality, jpeg artifacts, morbid, mutilated, out of frame, ugly, bad anatomy, bad proportions, deformed, blurry, duplicate"
+
+    for i in range(num_output):
+        output = model.generate(
+            samples,
+            seed=iter_seed + i,
+            guidance_scale=guidance_scale,
+            num_inference_steps=num_inference_steps,
+            neg_prompt=negative_prompt,
+            height=512,
+            width=512,
+        )
+
+        img = output[0]
+
+        img.save(f"./result_img/mit_mascot_bsz_2_with_irrel_and_rel/checkpoint_200/img_{i}.png")
+
+    torch.cuda.empty_cache()
 
 if __name__ == "__main__":
-    args = parse_args()
+    args = infer_parse_args()
     inference(args)
--- inference.sh
+++ inference.sh
@@ -4,20 +4,11 @@ export PRETRAINED_BLIP_DIFFUSION="https://storage.googleapis.com/sfr-vision-lang
 export INSTANCE_DIR="/LAVIS/data/mit_mascot"
 export OUTPUT_DIR="./output/with_without_irrel"
 
-accelerate launch train.py \
+python inference.py \
+  --finetuned_ckpt="/LAVIS/multi_BlipDisenBooth/output/mit_mascot_bsz_2_with_irrel_and_rel/checkpoint-200/model.safetensors" \
   --pretrained_model_name_or_path=$MODEL_NAME  \
   --pretrained_BLIPdiffusion_name_or_path=$PRETRAINED_BLIP_DIFFUSION \
-  --instance_data_dir=$INSTANCE_DIR \
-  --output_dir=$OUTPUT_DIR \
-  --subject_text="mit_mascot" \
+  --cond_subject="mit_mascot" \
+  --tgt_subject="mit_mascot" \
   --resolution=512 \
-  --train_batch_size=1 \
-  --gradient_accumulation_steps=1 \
-  --checkpointing_steps=20 \
-  --learning_rate=1e-4 \
-  --lr_scheduler="constant" \
-  --lr_warmup_steps=0 \
-  --max_train_steps=200 \
-  --validation_prompt="A mit_mascot in the jungle" \
-  --validation_epochs=20 \
   --seed="0" \
